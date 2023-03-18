1cd7d81a21bcde2cfe5af073ed0318ab18f0b400
Your <simon>
2023-03-18 07:11:30 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints a line with function 
- * return:0(Success)
+ * main - this is the main function 
+ *
+ * Return: always 0
  */
 
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
