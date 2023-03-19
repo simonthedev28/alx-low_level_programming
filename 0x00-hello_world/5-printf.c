493007431b3ee02f19d790379b9520b90c590459
Your <simon>
2023-03-18 17:19:49 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- *main - this is the main function
- *Return : 0 when successfull
+*main - Entry
+*Return : Always 0
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return ();
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
